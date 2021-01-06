2d7882fbea9df97e6ddeba2401eeef3204ab11f4
nandam1231 <yuco109@gmail.com>
2021-01-06 16:51:22 +0900

    stair-revise
    -anim 수정.....못함...
    -stair 수정

---
+++ Assets/Scripts/Animation/MonsterAnimCtrl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAnimCtrl : AnimCtrl
+{
+    public enum eAnimState
+    {
+        IDLE,
+        RUN,
+        ATTACK,
+        DIE,
+        MAX
+    }
+    eAnimState m_state;
+    Dictionary<int, eAnimState> m_dicAnimState = new Dictionary<int, eAnimState>();
+
+    public eAnimState GetAnimState()
+    {
+        return m_state;
+    }
+
+    public eAnimState GetAnimState(eAnimState state)
+    {
+        int hash = GetAnimHash(state);
+        return m_dicAnimState[hash];
+    }
+
+    public int GetAnimHash(eAnimState state)
+    {
+        return Animator.StringToHash(state.ToString());
+    }
+
+    public void Play(eAnimState animState, bool isFade = true)
+    {
+        m_state = animState;
+        base.Play(animState.ToString(), isFade);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<(int)eAnimState.MAX; i++)
+        {
+            m_dicAnimState.Add(Animator.StringToHash(((eAnimState)i).ToString()), (eAnimState)i);
+        }
+    }
+
+    
+}
--- Assets/Scripts/Zombie/Enemy.cs
+++ Assets/Scripts/Zombie/Enemy.cs
@@ -17,6 +17,12 @@ public class Enemy : MonoBehaviour
     protected float distance;
     protected bool canAttack = true;
 
+    [SerializeField]
+    private float m_maxHP;
+    public float m_currentHP;
+    public bool m_isDead;
+    public bool m_isHit = false;
+
     public LayerMask m_layerMask;
 
     // Start is called before the first frame update
@@ -27,8 +33,26 @@ public class Enemy : MonoBehaviour
         m_collider = GetComponent<CapsuleCollider>();
         m_nav = GetComponent<NavMeshAgent>();
         m_Anim = GetComponent<Animator>();
-
+        m_currentHP = m_maxHP;
         StartCoroutine(CalcCoolTime());
+    }
+
+    public void Hit(float damage)
+    {
+        m_currentHP -= damage;
+        if (m_currentHP > 0)
+        {
+            m_isHit = true;
+        }
+
+        if (m_currentHP <= 0 && !m_isDead)
+        {
+            m_Anim.SetTrigger("DEATH");
+            m_isDead = true;
+            m_collider.isTrigger = true;
+            m_isHit = false;
+            StopAllCoroutines();
+        }
     }
 
     protected bool CanAttackState()
