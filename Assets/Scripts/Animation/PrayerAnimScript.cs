4bd9cf9b11f442f324dd318d6569b3f84e254686
nandam1231 <yuco109@gmail.com>
2021-02-23 22:57:31 +0900

    prayerAnim
    -prayerAnim 추가
    -pink_woman, young_woman Anim 완료

---
+++ Assets/Scripts/Animation/PrayerAnimScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrayerAnimScript : MonoBehaviour
+{
+    private Animator m_Anim;
+    private bool isPraying = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // m_Anim.SetBool("PRAY", true);
+        AnimatorStateInfo info = m_Anim.GetCurrentAnimatorStateInfo(0);
+        isPraying = info.IsName("PRAY");
+
+        if (isPraying)
+        {
+            m_Anim.SetTrigger("PRAY");
+        }
+
+    }
+}
