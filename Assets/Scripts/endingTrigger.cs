0a545bf1afa53cf6e8635c13dabbaabe07597533
nandam1231 <yuco109@gmail.com>
2022-02-18 16:58:15 +0900

    normal_happy_ending
    - happy ending 추가
    - happy ending 스토리 짜야함
    - 2층 문 여는 것 작업해야함

--- Assets/Scripts/endingTrigger.cs
+++ Assets/Scripts/endingTrigger.cs
@@ -5,23 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class endingTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //ending이 2가지
+    //1. Normal ending -> 좀비 치료제 없이 그냥 탈출 -> clear ending Scene으로 전환 (이전과 동일)
+    //2. Happly ending -> 좀비 치료제를 가지고 탈출 -> happy ending Scene으로 전환
+    
+    [SerializeField]
+    private ActionController Player;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("ClearEnding");
+            for (int i = 0; i < Player.inventory.GetSlots.Length; i++)
+            {
+                //만약 player의 인벤토리에 RedSyringe가 있다면 Happy Ending
+                if (Player.inventory.GetSlots[i].item.Id == 0)
+                {
+                    SceneManager.LoadScene("HappyEnding");
+                }
+                else
+                {
+                    //없다면 Normal Ending
+                    SceneManager.LoadScene("ClearEnding");
+                }
+            }
         }
     }
+
+
 }
