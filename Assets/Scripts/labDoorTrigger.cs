e1ed928fcb8f2358f81ce7b2b5c087e901c4b281
hanjiyeon <jiyeon53885@gmail.com>
2022-03-27 02:11:57 +0900

    실험실 문 애니메이션

---
+++ Assets/Scripts/labDoorTrigger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class labDoorTrigger : MonoBehaviour
+{
+    
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player enter");
+            animator.SetBool("isOpen", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player enter");
+            animator.SetBool("isOpen", false);
+        }
+    }
+}
